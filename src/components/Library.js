e9e4d338e600742281acff222828c449869be899
mkavo92 <mkavo92@gmail.com>
2018-05-11 23:10:18 -0500

    add those

---
+++ src/components/Library.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import albumData from './../data/albums';
+class Library extends Component {
+  constructor(props){
+    super(props);
+    this.state = { albums: albumData };
+  }
+  render() {
+    return (
+      <section className = 'library' >
+      {
+        this.state.albums.map( (album, index) => 
+        <div key= {index} >
+        <img src ={album.albumCover} alt ={album.title} />
+        <div>{album.title}</div>
+        <div>{album.artist}</div>
+        <div>{album.songs.length} songs</div>
+        </div>
+        )
+      }
+  </section>
+);
+  }
+}
+
+export default Library;
