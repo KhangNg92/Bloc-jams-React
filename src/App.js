44acf1b20100a4f83b0b34edadb8f59d29929113
mkavo92 <mkavo92@gmail.com>
2018-05-12 20:48:34 -0500

    fix package

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
          <main>
          <Route exact path="/" component={Landing} /> {/* exact? By default, a route matches any path in the URL that begins with the value of the path prop*/}
            <Route path="/library" component={Library} />
-           <Routh path="/album:slug" component={Album} />
+           <Route path="/album:slug" component={album} />
          </main>
       </div>
     );
