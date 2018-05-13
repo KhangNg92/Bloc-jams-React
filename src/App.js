e636d1963b13c559663ac64af867175897195814
mkavo92 <mkavo92@gmail.com>
2018-05-12 21:24:55 -0500

    change app back

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Album from './components/Album';
 import './App.css';
 import { Route, Link } from 'react-router-dom';
 import Landing from './components/Landing';
-import Album from './components/Album';
 import Library from './components/Library';
 
 
@@ -23,18 +22,6 @@ class App extends Component {
            <Route path="/library" component={Library} />
            <Route path="/album/:slug" component={Album} />
          </main>
-      <header>
-        <nav>
-          <Link to='/'>Landing</Link>
-          <Link to='/library'>Library</Link>
-        </nav>
-        <h1>Bloc Jams</h1>
-       </header>
-       <main>
-        <Route exact path="/" component={Landing} />
-        <Route path="/album/:slug" component={Album} />
-        <Route path="/library" component={Library} />
-       </main>
       </div>
     );
   }
