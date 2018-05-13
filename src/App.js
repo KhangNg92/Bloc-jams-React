b185f58d6f70bb1c15d59cddcd6cbffa9e42f115
mkavo92 <mkavo92@gmail.com>
2018-05-12 21:22:10 -0500

    add app

--- src/App.js
+++ src/App.js
@@ -2,23 +2,26 @@ import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 import './App.css';
 import Landing from './components/Landing';
+import Album from './components/Album';
 import Library from './components/Library';
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <header>
-       <nav>
-             <Link to='/'>Landing</Link>
-             <Link to='/library'>Library</Link>
-           </nav>
-           <h1>Bloc Jams</h1>
-         </header>
-         <main>
-         <Route exact path="/" component={Landing} /> {/* exact? By default, a route matches any path in the URL that begins with the value of the path prop*/}
-           <Route path="/library" component={Library} />
-           <Routh path="/album:slug" component={Album} />
-         </main>
+      <header>
+        <nav>
+          <Link to='/'>Landing</Link>
+          <Link to='/library'>Library</Link>
+        </nav>
+        <h1>Bloc Jams</h1>
+       </header>
+       <main>
+        <Route exact path="/" component={Landing} />
+        <Route path="/album/:slug" component={Album} />
+        <Route path="/library" component={Library} />
+       </main>
       </div>
     );
   }
