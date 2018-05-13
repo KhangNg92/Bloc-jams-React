bac08020a18e42432a5b59220cbbf8e59cfe9e6a
mkavo92 <mkavo92@gmail.com>
2018-05-12 21:32:04 -0500

    Library success

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import albumData from './../data/albums';
+import albumData from './../data/Album';
 class Library extends Component {
   constructor(props){
     super(props);
