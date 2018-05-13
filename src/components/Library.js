d5f6d0370d2d61690b140da89f4ac6d83cb9c743
mkavo92 <mkavo92@gmail.com>
2018-05-13 12:05:41 -0500

    fix library

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import albumData from './../data/Album';
+import albumData from './../data/album';
 class Library extends Component {
   constructor(props){
     super(props);
