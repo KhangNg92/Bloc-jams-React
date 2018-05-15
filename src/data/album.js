7bcc3ca375de4785f3b60891d0e8346f87ebb972
mkavo92 <mkavo92@gmail.com>
2018-05-15 12:53:44 -0500

    style assignment

--- src/data/album.js
+++ src/data/album.js
@@ -18,7 +18,7 @@ export default [{
       albumCover: '/assets/images/album_covers/02.jpg',
       slug: 'the-telephone',
       songs: [
-        { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
+        { title: 'Blue', duration: '2:41', audioSrc: '/assets/music/blue.mp3' },
         { title: 'Green', duration: '1:43', audioSrc: '/assets/music/green.mp3' },
         { title: 'Red', duration: '4:28', audioSrc: '/assets/music/red.mp3' },
         { title: 'Pink', duration: '2:33', audioSrc: '/assets/music/pink.mp3' },
